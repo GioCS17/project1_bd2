979a38fa3e37b16ff177b3f31324ccb12a2cf0ba
gio <giordano.alvitez@utec.edu.pe>
2020-05-02 22:36:07 -0500

    feat(ManageDisk): write and retrieve operation for disk

---
+++ controldisk.h
@@ -0,0 +1,45 @@
+
+#include<fstream>
+#include<iostream>
+#include<string>
+
+namespace bd2{
+class controldisk : protected std::fstream{
+
+  std::string filePath;
+  bool empty;
+
+
+  public:
+
+  controldisk(std::string fp,bool reset); : filePath(fp), std::fstrema(fp.data(), std::ios::in | std::ios::out | std::ios::binary){
+    reset=false;
+
+    if(!good() || reset){ //good check if any flag bit without googbit is on
+      empty=true;
+      open(fp.data(),std::fstrema(fp.data(), std::ios::in | std::ios::out | std::ios::binary){
+
+    }
+  }
+  ~controldisk(){ close();}
+
+
+  template<typename Record>
+  void write(const long &n, Record& reg){
+    clear(); //reset flags bit (goodbit, eofbit, failbit, badbit)
+    seekp(n*sizeof(Record),std::ios::beg);
+    write(reinterpret_cast<const char*>(*reg),sizeof(reg));
+
+  }
+
+  template<typename Record>
+  bool retrieve(const long &n, Record& reg){
+    clear();
+    seekg(n*sizeof(Record),std::ios::beg);
+    read(reinterpret_cast<char *>(&reg),sizeof(reg));
+    return gcount()>0; //Returns the number of characters extracted by the last unformatted input operation performed on the fstrem .
+  }
+
+
+};
+}
